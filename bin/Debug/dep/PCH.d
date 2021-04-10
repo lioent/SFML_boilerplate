b887af7095d3ecbacf69f3db6b4d97750c999833
lionet <callmelionet@gmail.com>
2021-04-09 22:00:55 -0300

    Initial commit

---
+++ bin/Debug/dep/PCH.d
@@ -0,0 +1,315 @@
+bin/Debug/obj/PCH.hpp.gch: src/PCH.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio.hpp \
+ E:/SFML-2.5.1/include/SFML/System.hpp \
+ E:/SFML-2.5.1/include/SFML/Config.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Clock.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Export.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Time.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Err.hpp \
+ E:/SFML-2.5.1/include/SFML/System/FileInputStream.hpp \
+ E:/SFML-2.5.1/include/SFML/System/InputStream.hpp \
+ E:/SFML-2.5.1/include/SFML/System/NonCopyable.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Lock.hpp \
+ E:/SFML-2.5.1/include/SFML/System/MemoryInputStream.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Mutex.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Sleep.hpp \
+ E:/SFML-2.5.1/include/SFML/System/String.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Utf.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Utf.inl \
+ E:/SFML-2.5.1/include/SFML/System/String.inl \
+ E:/SFML-2.5.1/include/SFML/System/Thread.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Thread.inl \
+ E:/SFML-2.5.1/include/SFML/System/ThreadLocal.hpp \
+ E:/SFML-2.5.1/include/SFML/System/ThreadLocalPtr.hpp \
+ E:/SFML-2.5.1/include/SFML/System/ThreadLocalPtr.inl \
+ E:/SFML-2.5.1/include/SFML/System/Vector2.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Vector2.inl \
+ E:/SFML-2.5.1/include/SFML/System/Vector3.hpp \
+ E:/SFML-2.5.1/include/SFML/System/Vector3.inl \
+ E:/SFML-2.5.1/include/SFML/Audio/InputSoundFile.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/Export.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/Listener.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/Music.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundStream.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundSource.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/AlResource.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/OutputSoundFile.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/Sound.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundBuffer.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundBufferRecorder.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundRecorder.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundFileFactory.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundFileFactory.inl \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundFileReader.hpp \
+ E:/SFML-2.5.1/include/SFML/Audio/SoundFileWriter.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics.hpp \
+ E:/SFML-2.5.1/include/SFML/Window.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Clipboard.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Export.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Context.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/GlResource.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/ContextSettings.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Cursor.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Event.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Joystick.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Keyboard.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Mouse.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Sensor.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Touch.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/VideoMode.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/Window.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/WindowHandle.hpp \
+ E:/SFML-2.5.1/include/SFML/Window/WindowStyle.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/BlendMode.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Export.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/CircleShape.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Shape.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Drawable.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/RenderStates.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Transform.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Rect.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Rect.inl \
+ E:/SFML-2.5.1/include/SFML/Graphics/Transformable.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/VertexArray.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Vertex.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Color.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/PrimitiveType.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/ConvexShape.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Font.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Glyph.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Texture.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Image.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/RectangleShape.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/RenderTarget.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/View.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/RenderTexture.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/RenderWindow.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Shader.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Glsl.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Glsl.inl \
+ E:/SFML-2.5.1/include/SFML/Graphics/Sprite.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/Text.hpp \
+ E:/SFML-2.5.1/include/SFML/Graphics/VertexBuffer.hpp \
+ E:/SFML-2.5.1/include/SFML/Network.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/Ftp.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/Export.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/TcpSocket.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/Socket.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/SocketHandle.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/Http.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/IpAddress.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/Packet.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/SocketSelector.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/TcpListener.hpp \
+ E:/SFML-2.5.1/include/SFML/Network/UdpSocket.hpp \
+ src/Utility/FileSystem.hpp src/Utility/Types.hpp
+
+E:/SFML-2.5.1/include/SFML/Audio.hpp:
+
+E:/SFML-2.5.1/include/SFML/System.hpp:
+
+E:/SFML-2.5.1/include/SFML/Config.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Clock.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Export.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Time.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Err.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/FileInputStream.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/InputStream.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/NonCopyable.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Lock.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/MemoryInputStream.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Mutex.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Sleep.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/String.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Utf.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Utf.inl:
+
+E:/SFML-2.5.1/include/SFML/System/String.inl:
+
+E:/SFML-2.5.1/include/SFML/System/Thread.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Thread.inl:
+
+E:/SFML-2.5.1/include/SFML/System/ThreadLocal.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/ThreadLocalPtr.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/ThreadLocalPtr.inl:
+
+E:/SFML-2.5.1/include/SFML/System/Vector2.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Vector2.inl:
+
+E:/SFML-2.5.1/include/SFML/System/Vector3.hpp:
+
+E:/SFML-2.5.1/include/SFML/System/Vector3.inl:
+
+E:/SFML-2.5.1/include/SFML/Audio/InputSoundFile.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/Export.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/Listener.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/Music.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundStream.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundSource.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/AlResource.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/OutputSoundFile.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/Sound.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundBuffer.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundBufferRecorder.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundRecorder.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundFileFactory.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundFileFactory.inl:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundFileReader.hpp:
+
+E:/SFML-2.5.1/include/SFML/Audio/SoundFileWriter.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Clipboard.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Export.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Context.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/GlResource.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/ContextSettings.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Cursor.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Event.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Joystick.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Keyboard.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Mouse.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Sensor.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Touch.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/VideoMode.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/Window.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/WindowHandle.hpp:
+
+E:/SFML-2.5.1/include/SFML/Window/WindowStyle.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/BlendMode.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Export.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/CircleShape.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Shape.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Drawable.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/RenderStates.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Transform.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Rect.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Rect.inl:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Transformable.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/VertexArray.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Vertex.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Color.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/PrimitiveType.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/ConvexShape.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Font.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Glyph.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Texture.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Image.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/RectangleShape.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/RenderTarget.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/View.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/RenderTexture.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/RenderWindow.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Shader.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Glsl.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Glsl.inl:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Sprite.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/Text.hpp:
+
+E:/SFML-2.5.1/include/SFML/Graphics/VertexBuffer.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/Ftp.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/Export.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/TcpSocket.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/Socket.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/SocketHandle.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/Http.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/IpAddress.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/Packet.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/SocketSelector.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/TcpListener.hpp:
+
+E:/SFML-2.5.1/include/SFML/Network/UdpSocket.hpp:
+
+src/Utility/FileSystem.hpp:
+
+src/Utility/Types.hpp:
